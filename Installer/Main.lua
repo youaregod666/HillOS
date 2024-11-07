f2c5d825b5b457bd250854f641064032b44c0cc2
youaregod666 <94928206+youaregod666@users.noreply.github.com>
2024-11-07 11:19:12 +0100

    Update Main.lua

--- Installer/Main.lua
+++ Installer/Main.lua
@@ -222,7 +222,7 @@ local paths = require("Paths")
 
 -- Creating main UI workspace
 local workspace = GUI.workspace()
-workspace:addChild(GUI.panel(1, 1, workspace.width, workspace.height, 0x1E1E1E))
+workspace:addChild(GUI.panel(1, 1, workspace.width, workspace.height, 0x336dbf))
 
 -- Main installer window
 local window = workspace:addChild(GUI.window(1, 1, 80, 24))
